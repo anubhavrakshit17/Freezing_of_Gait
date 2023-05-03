afcd8ad2e9fb04a22d47d40d0ac91cad052521ea
Anubhav Rakshit <38507672+anubhavrakshit17@users.noreply.github.com>
2023-05-03 12:00:49 +0200

    Add files via upload

--- functions/fn_save_gait_events_file.m
+++ functions/fn_save_gait_events_file.m
@@ -1,6 +1,15 @@
 function fn_save_gait_events_file()
     % Take input filename from user
     filename = input("Enter the gaitEvents file from the GaitData folder including '.mat': ", 's');
+    rf_file = [filename(1:end-4) '_rf.txt'];
+    lf_file = [filename(1:end-4) '_lf.txt'];
+    gaitdata_folder = fullfile('SenseFOG', 'GaitData');
+    
+    % Check if files already exist in GaitData folder
+    if exist(fullfile(gaitdata_folder, rf_file), 'file') == 2 && exist(fullfile(gaitdata_folder, lf_file), 'file') == 2
+        disp('Files already exist in GaitData folder, skipping writing');
+        return
+    end
 
     % Load the file
     load(fullfile('GaitData', filename));
@@ -28,9 +37,10 @@ function fn_save_gait_events_file()
 
     % Write the rf events to a file
     rf_events = table(rf_Midswing, rf_Swing_Start, rf_Swing_End, rf_Toe_Off, rf_Heelstrike);
-    writetable(rf_events, 'rf_event_file.txt', 'Delimiter', 'tab');
+    writetable(rf_events, fullfile(gaitdata_folder, rf_file), 'Delimiter', 'tab');
 
     % Write the lf events to a file
     lf_events = table(lf_Midswing, lf_Swing_Start, lf_Swing_End, lf_Toe_Off, lf_Heelstrike);
-    writetable(lf_events, 'lf_event_file.txt', 'Delimiter', 'tab');
+    writetable(lf_events, fullfile(gaitdata_folder, lf_file), 'Delimiter', 'tab');
 end
+
